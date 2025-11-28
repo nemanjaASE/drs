2ca5ab7fcb66635ea6f19fa1194db10d4a2a29d0
vladanaaleksic3 <vladanaaleksic3@gmail.com>
2025-11-28 22:19:10 +0100

    added interface with min attributes

--- Termin 03/frontend/src/components/Button.tsx
+++ Termin 03/frontend/src/components/Button.tsx
@@ -1,1 +1,10 @@
 //Reusable button
+import { type ReactNode, type ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "danger";
+  size?: "sm" | "md" | "lg";
+  //maybe an attribute if the btn will be at the left or right side of the text 
+  loading?: boolean;
+  children: ReactNode;
+}
